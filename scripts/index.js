f7fc46dea5d4fa443dd6867ef5cf958d9821e156
Artur Pluzhnikov <artur@artbox-btl.ru>
2022-02-01 23:14:45 +0300

    second iteration

--- scripts/index.js
+++ scripts/index.js
@@ -1,34 +1,28 @@
-let formOpenButton = document.querySelector('.profile__edit-button')
-let form = document.querySelector('.form')
-let formCloseButton = document.querySelector('.form__close')
-let formSubmit = document.querySelector('.form__button')
-let nameInput = document.querySelector('.form__data_name')
-let jobInput = document.querySelector('.form__data_description')
-let nameOutput = document.querySelector('.profile__title')
-let jobOutput = document.querySelector('.profile__subtitle')
+let popupOpenButton = document.querySelector('.profile__edit-button');
+let popup = document.querySelector('.popup');
+let popupCloseButton = document.querySelector('.popup__close-button');
+let nameInput = document.querySelector('.form__data-container_name');
+let jobInput = document.querySelector('.form__data-container_description');
+let nameOutput = document.querySelector('.profile__title');
+let jobOutput = document.querySelector('.profile__subtitle');
 
-function openForm() {
-  nameInput.value = nameOutput.textContent
-  jobInput.value = jobOutput.textContent
-  form.classList.add('form__opened')
+function openPopup() {
+  nameInput.value = nameOutput.textContent;
+  jobInput.value = jobOutput.textContent;
+  popup.classList.add('popup__opened');
 };
 
-formOpenButton.addEventListener('click', openForm)
-
-function closeForm(evt) {
-  evt.preventDefault();
-  form.classList.remove('form__opened')
-}
-
-formCloseButton.addEventListener('click', closeForm)
-
+function closePopup(evt) {
+  popup.classList.remove('popup__opened');
+};
 
 function formSubmitHandler(evt) {
   evt.preventDefault();
     nameOutput.textContent = nameInput.value;
     jobOutput.textContent = jobInput.value;
     closeForm(evt);
-}
+};
 
 form.addEventListener('submit', formSubmitHandler); 
-
+popupCloseButton.addEventListener('click', closePopup);
+popupOpenButton.addEventListener('click', openPopup);
